f1739bb19abfcb0c68106fffa20b65d7aef01835
Cha Hoseong <hoseong@chahoseong.com>
2026-01-26 19:13:35 +0900

    anim instance base class

---
+++ Blademaster/Animation/BlademasterAnimInstance.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BlademasterAnimInstance.generated.h"
+
+UCLASS()
+class BLADEMASTER_API UBlademasterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
